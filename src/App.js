ecdada0bb452b33ff1051ac5d09192f520910f2c
Daniyal Abbas <d.abbas1800@gmail.com>
2022-08-22 13:39:54 +0100

    Exercise

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './bbsWheel.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exercise done, im the best
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="https://dzza1800.github.io/myPortfolio/HomePage.html" className="App-link" target="_blank" rel="noopener noreferrer"> The link to my portfolio </a>
       </header>
     </div>
   );
