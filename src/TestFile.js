f48e0cc96272f8bceff6c166ae437aec4e38dcd5
Daniyal Abbas <d.abbas1800@gmail.com>
2022-08-23 14:03:39 +0100

    Component

---
+++ src/TestFile.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+
+function idk(){
+    return(
+    <footer>
+        <>
+        Hi I like Potatoes
+        </>
+        <br />
+        <>
+        And margerine
+        </>
+        <br />
+        <>
+        And a Ferrari
+        </>
+        <br />
+        <>
+        But I'm poor
+        </>
+
+    </footer>
+
+    );
+}
+
+function inheritFunc(){
+    return idk();
+}
+
+export default inheritFunc;
