717dcf7b1c638ccc289a09c35f9ce08180388f4c
Daniyal Abbas <d.abbas1800@gmail.com>
2022-08-23 21:38:25 +0100

    added prop

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Proppy from './newComponent.jsx';
 import TestFile from './TestFile.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <><h1>My Story</h1></>
-    <TestFile />
+    <Proppy />
   </React.StrictMode>
 );
 
---
+++ src/newComponent.jsx
@@ -0,0 +1,14 @@
+function PropsTings(props){
+
+    return <>
+    <h1>The name is: {props.name}</h1>
+    </>
+}
+
+function PropsFire(){
+    return (<>
+    <PropsTings name="dhsdhawhd"/>
+    </>);
+}
+
+export default PropsFire;
